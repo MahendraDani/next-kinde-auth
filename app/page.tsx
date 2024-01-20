f27b6b1b438221da817317be537cb03661912741
Mahendra Dani <danimahendra0904@gmail.com>
2024-01-20 14:35:09 +0530

    feat: logged in users are redirect to dashboard on going to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const { isAuthenticated } = getKindeServerSession();
+  const isAuthed = await isAuthenticated();
+  if (isAuthed) {
+    redirect("/dashboard")
+  }
   return (
     <main className=''>
       <div className='flex flex-col gap-4 justify-between'>
