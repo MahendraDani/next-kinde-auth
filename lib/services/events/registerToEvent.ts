98f2585c7a8a141460597431262b25c44bafbc6c
Mahendra Dani <danimahendra0904@gmail.com>
2024-01-22 11:24:36 +0530

    fix: add event_id contraint in geting registrants count query

--- lib/services/events/registerToEvent.ts
+++ lib/services/events/registerToEvent.ts
@@ -3,7 +3,7 @@ import { Attendees } from "@prisma/client";
 
 export const registerToEvent = async ({
   event_id,
-  user_id
+  user_id,
 }: Pick<Attendees, "event_id" | "user_id">) => {
   try {
     // Adds the new registrant to attendees table
@@ -13,25 +13,26 @@ export const registerToEvent = async ({
         event_id,
         user_id,
       },
-    })
+    });
 
     // gets the total number of registrants after insertion
     const registrants = await prisma.attendees.count({
       where: {
+        event_id,
         is_present: false,
-      }
-    })
+      },
+    });
 
     // Updates the regitrants_count field in Event table
     const increment_registrants_count = await prisma.event.update({
       data: {
-        registrants_count: registrants
+        registrants_count: registrants,
       },
       where: {
-        event_id
-      }
-    })
+        event_id,
+      },
+    });
   } catch (error) {
     console.log(error);
   }
-}
+};
