2abe918e330943b97be3275774f1de4dd13615d5
Mahendra Dani <danimahendra0904@gmail.com>
2024-01-22 09:38:48 +0530

    bug: registrants count is now handled when a user un-register's from an event

--- lib/services/events/unregisterFromEvent.ts
+++ lib/services/events/unregisterFromEvent.ts
@@ -1,14 +1,30 @@
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 
-export const unregisterFromEvent = async ({ event_id, user_id }: { event_id: string, user_id: string }) => {
+export const unregisterFromEvent = async ({
+  event_id,
+  user_id,
+}: {
+  event_id: string;
+  user_id: string;
+}) => {
   try {
+    const prev_registrants_count = await prisma.attendees.count();
     const response = await prisma.attendees.delete({
       where: {
         id: `${event_id}-${user_id}`,
         is_present: false,
-      }
-    })
+      },
+    });
+
+    const update_registrants_count = await prisma.event.update({
+      data: {
+        registrants_count: prev_registrants_count - 1,
+      },
+      where: {
+        event_id,
+      },
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
